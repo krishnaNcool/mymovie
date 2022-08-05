6b0d8e7378359cab94b6c71ea8f519fbc8445f91
krishna N <krishnannagesh81@gmail.com>
2022-08-05 10:33:14 +0530

    ok

---
+++ src/test/java/com/book/mymovie/MymovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.mymovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
